3993d85b2c6070c2ed12b269e5b01da81bfd59cf
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2021-04-02 19:56:24 +0530

    Tailwind CSS installed and tested

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React from 'react';
-import { Link } from "react-router-dom"
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>React Electron Boilerplate</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Link className="App-link" to="/about">Link to the About Page</Link>
-      </header>
-      
+    <div className="bg-black h-screen flex items-center">
+		<div className="mx-auto flex justify-center">
+			<button className="px-4 py-1 bg-green-400 text-white rounded">Click</button>
+		</div>
+        {/* <Link className="App-link" to="/about">Link to the About Page</Link> */}
     </div>
   );
 }
